bc9ba6b08406321d74658398fccd35072ea765a0
JonyLucas <jony.lucas.br13@gmail.com>
2016-06-28 08:15:43 +0000

    Add files via upload

---
+++ No.java
@@ -0,0 +1,32 @@
+package projetolavid;
+
+public class No {
+
+	private int conteudo;
+	private No proximo;
+	
+	public No(int valor){
+		conteudo = valor;
+		proximo = null;
+	}
+	
+	public No getProx(){
+		
+		return proximo;
+	}
+	
+	public void setProx(No proxNo){
+		this.proximo = proxNo;
+		
+	}
+	
+	public int getDado(){
+		
+		return this.conteudo;
+	}
+	
+	public void setDado(int dado){
+		
+		this.conteudo = dado;
+	}
+}
